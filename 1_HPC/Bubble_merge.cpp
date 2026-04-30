f5873dd188667380c38cb33c03e3dec733d30377
NehaKulkarni28 <158441391+Nehak89@users.noreply.github.com>
2026-04-30 19:41:07 +0530

    Update Bubble_merge.cpp

--- 1_HPC/Bubble_merge.cpp
+++ 1_HPC/Bubble_merge.cpp
@@ -3,97 +3,68 @@
 #include <omp.h>
 using namespace std;
 
-// Bubble Sort
-void bubble(vector<int> &a, int n)
-{
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
-            if (a[j] > a[j + 1])
-                swap(a[j], a[j + 1]);
+// Bubble
+void b(vector<int>& a){
+    for(int i=0;i<a.size()-1;i++)
+        for(int j=0;j<a.size()-i-1;j++)
+            if(a[j]>a[j+1]) swap(a[j],a[j+1]);
 }
 
-void pbubble(vector<int> &a, int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-#pragma omp parallel for
-        for (int j = 0; j < n - i - 1; j++)
-            if (a[j] > a[j + 1])
-                swap(a[j], a[j + 1]);
+void pb(vector<int>& a){
+    for(int i=0;i<a.size()-1;i++)
+    #pragma omp parallel for
+        for(int j=0;j<a.size()-i-1;j++)
+            if(a[j]>a[j+1]) swap(a[j],a[j+1]);
+}
+
+// Merge
+void m(vector<int>& a,int l,int r){
+    if(l<r){
+        int m1=(l+r)/2;
+        m(a,l,m1); m(a,m1+1,r);
+        vector<int> t; int i=l,j=m1+1;
+        while(i<=m1 && j<=r)
+            t.push_back(a[i]<a[j]?a[i++]:a[j++]);
+        while(i<=m1) t.push_back(a[i++]);
+        while(j<=r) t.push_back(a[j++]);
+        for(int k=0;k<t.size();k++) a[l+k]=t[k];
     }
 }
 
-// Merge Sort
-void merge(vector<int> &a, int l, int m, int r)
-{
-    vector<int> t;
-    int i = l, j = m + 1;
-    while (i <= m && j <= r)
-        t.push_back(a[i] < a[j] ? a[i++] : a[j++]);
-    while (i <= m)
-        t.push_back(a[i++]);
-    while (j <= r)
-        t.push_back(a[j++]);
-    for (int k = 0; k < t.size(); k++)
-        a[l + k] = t[k];
-}
-
-void msort(vector<int> &a, int l, int r)
-{
-    if (l < r)
-    {
-        int m = (l + r) / 2;
-        msort(a, l, m);
-        msort(a, m + 1, r);
-        merge(a, l, m, r);
-    }
-}
-
-void pmsort(vector<int> &a, int l, int r)
-{
-    if (l < r)
-    {
-        int m = (l + r) / 2;
-#pragma omp parallel sections
+void pm(vector<int>& a,int l,int r){
+    if(l<r){
+        int m1=(l+r)/2;
+        #pragma omp parallel sections
         {
-#pragma omp section
-            pmsort(a, l, m);
-#pragma omp section
-            pmsort(a, m + 1, r);
+            #pragma omp section
+            pm(a,l,m1);
+            #pragma omp section
+            pm(a,m1+1,r);
         }
-        merge(a, l, m, r);
+        m(a,l,r);
     }
 }
 
-int main()
-{
-    int n;
-    cin >> n;
-    vector<int> a(n), b, c, d;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    b = c = d = a;
+int main(){
+    int n; cin>>n;
+    vector<int>a(n),b1,c,d;
+    for(int i=0;i<n;i++) cin>>a[i];
+    b1=c=d=a;
 
-    double t1 = omp_get_wtime();
-    bubble(b, n);
-    double t2 = omp_get_wtime();
-    double t3 = omp_get_wtime();
-    pbubble(c, n);
-    double t4 = omp_get_wtime();
-    double t5 = omp_get_wtime();
-    msort(a, 0, n - 1);
-    double t6 = omp_get_wtime();
-    double t7 = omp_get_wtime();
-    pmsort(d, 0, n - 1);
-    double t8 = omp_get_wtime();
+    double t;
 
-    cout << "\nBub Seq: " << t2 - t1;
-    cout << "\nBub Par: " << t4 - t3;
-    cout << "\nMer Seq: " << t6 - t5;
-    cout << "\nMer Par: " << t8 - t7;
-}
+    t=omp_get_wtime(); b(b1);
+    cout<<"Bub Seq "<<omp_get_wtime()-t<<"\n";
 
-/*
+    t=omp_get_wtime(); pb(c);
+    cout<<"Bub Par "<<omp_get_wtime()-t<<"\n";
+
+    t=omp_get_wtime(); m(a,0,n-1);
+    cout<<"Mer Seq "<<omp_get_wtime()-t<<"\n";
+
+    t=omp_get_wtime(); pm(d,0,n-1);
+    cout<<"Mer Par "<<omp_get_wtime()-t;
+}/*
 Input-
 Enter size : 5
 Enter elements : 5 3 1 4 2
